45f78e399308e1463113d0a6077f3fc92f86eb00
Jan Matoušek <jan.matousek@rekomando.cz>
2020-08-30 21:04:11 +0200

    Fix door filling - there should be no sqrt.

--- Dungeon/Room.cpp
+++ Dungeon/Room.cpp
@@ -2,7 +2,7 @@
 
 void Room::GenerateDoors(int count, std::mt19937 &gen) {
     int findX, findY;
-    for (int i = 0; i < std::sqrt(count); i++) {
+    for (int i = 0; i < count; i++) {
         if(FindRandomTile(0, TileType::WALL, findX, findY, gen)) {
             doors.push_back({ findX, findY, Rotation::D0 });
         }
