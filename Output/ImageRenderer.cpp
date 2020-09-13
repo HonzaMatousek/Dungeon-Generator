5abe2d6d7c68b7486cac03f6728a509baba0282d
Jan Matoušek <jan.matousek@rekomando.cz>
2020-09-13 21:41:21 +0200

    Use tile sprites for dungeon image generation.

--- Output/ImageRenderer.cpp
+++ Output/ImageRenderer.cpp
@@ -1,37 +1,53 @@
 #include "ImageRenderer.h"
 #include <fstream>
 
-ImageRenderer::ImageRenderer(const std::string & outputFileName) : outputFileName(outputFileName) {}
+ImageRenderer::ImageRenderer(const std::string & outputFileName) : outputFileName(outputFileName) {
+    std::ifstream tilesFile("dungeon-tiles.tga", std::ios::binary);
+    char TGAheader[18];
+    tilesFile.read(TGAheader, sizeof(TGAheader));
+    tiles.resize(64 * 64 * 3);
+    tilesFile.read(tiles.data(), tiles.size());
+}
 
 void ImageRenderer::Render(const Dungeon &dungeon) {
     std::ofstream outputFile(outputFileName, std::ios::binary);
-    unsigned char TGAheader[18] = {0,0,2, 0,0, 0,0, 16, 0,0, 0,0, (unsigned char)(dungeon.getWidth() & 0xFFu), (unsigned char)(dungeon.getWidth() >> 8u), (unsigned char)(dungeon.getHeight() & 0xFFu), (unsigned char)(dungeon.getHeight() >> 8u), 24, 0x20};
+    unsigned char TGAheader[18] = {0,0,2, 0,0, 0,0, 16, 0,0, 0,0, (unsigned char)((16 * dungeon.getWidth()) & 0xFFu), (unsigned char)((16 * dungeon.getWidth()) >> 8u), (unsigned char)((16 * dungeon.getHeight()) & 0xFFu), (unsigned char)((16 * dungeon.getHeight()) >> 8u), 24, 0x20};
     outputFile.write((char *)TGAheader, sizeof(TGAheader));
-    dungeon.WalkTiles([&] (const TileCoord & tileCoord) {
-        switch(dungeon.at(tileCoord).type) {
-            case TileType::WALL:
-                if(dungeon.at(tileCoord).roomNumber == -1) {
-                    outputFile.write("\xcc\xcc\xff", 3);
+    for (int row = 0; row < dungeon.getHeight(); row++) {
+        for(int line = 0; line < 16; line++) {
+            for (int col = 0; col < dungeon.getWidth(); col++) {
+                int spriteX = 0;
+                int spriteY = 0;
+                switch (dungeon.at(col, row).type) {
+                    case TileType::WALL:
+                        if (dungeon.CountNeighbors8({col, row}, TileType::WALL, true) > 7) {
+                            spriteX = 0;
+                            spriteY = 48;
+                        }
+                        else {
+                            spriteX = 0;
+                            spriteY = 0;
+                        }
+                        break;
+                    case TileType::FLOOR:
+                        spriteX = 0;
+                        spriteY = 16;
+                        break;
+                    case TileType::DOOR:
+                        spriteX = 0;
+                        spriteY = 32;
+                        break;
+                    case TileType::MONSTER:
+                        spriteX = 16;
+                        spriteY = 0;
+                        break;
+                    case TileType::CHEST:
+                        spriteX = 16;
+                        spriteY = 16;
+                        break;
                 }
-                else if(dungeon.CountNeighbors8(tileCoord, TileType::WALL, true) > 7) {
-                    outputFile.write("\xff\xff\xff", 3);
-                }
-                else {
-                    outputFile.write("\x00\x00\x00", 3);
-                }
-                break;
-            case TileType::FLOOR:
-                outputFile.write("\xcc\xcc\xcc", 3);
-                break;
-            case TileType::DOOR:
-                outputFile.write("\x99\x99\x99", 3);
-                break;
-            case TileType::MONSTER:
-                outputFile.write("\x00\x00\xff", 3);
-                break;
-            case TileType::CHEST:
-                outputFile.write("\xee\xee\x00", 3);
-                break;
+                outputFile.write(tiles.data() + ((spriteY + line) * 64 + spriteX) * 3, 16 * 3);
+            }
         }
-    });
+    }
 }
--- Output/ImageRenderer.h
+++ Output/ImageRenderer.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include "Renderer.h"
 
 class ImageRenderer : public Renderer {
     std::string outputFileName;
+    std::vector<char> tiles;
 public:
     explicit ImageRenderer(const std::string & outputFileName);
     void Render(const Dungeon & dungeon) override;
